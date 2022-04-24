2214f9a4191f7adf653ba62e0b8b6241adf4e2e4
luisloopz <luisgustavo785@hotmail.com>
2022-04-23 23:29:04 -0300

    primeiro site .php

---
+++ pagina.php
@@ -0,0 +1,54 @@
+<html lang="pt-br">
+<head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <title>Primeiro site em PHP!</title>
+   <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+   <link rel="stylesheet" href="/resources/demos/style.css">
+   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+   <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
+   <script>
+       $( function() {
+           $( "#accordion" ).accordion();
+       });
+       </script>
+       </head>
+       <body>
+       
+       <div id="accordion">
+           <h3>O que estou achando da transição?</h3>
+           <div>
+               <p>
+                Muito bom! finalmente achei algo que sinto prazer em fazer, em estudar em procurar aprender. esta sendo gratificante esta transição.
+               </p>
+    </div>
+    <h3>Qual stack estou achando mais complexa?</h3>
+    <div>
+        <p>
+            Até então estou achando mais complexa o back-end mas acho que é nele que vou investir pois acho que não sou muito pra design
+    </p>
+    </div>
+    <h3>O que estou achando do bootcamp?</h3>
+    <div>
+        <p>
+            O bootcamp está sendo ótimo para o meu aprendizado, todos os cursos e desafios estão sendo inovadores e marcantes. Volto agradecer ao pessoal da DIO e do Carrefour por proporcionar
+            um curso com excelente qualidade.
+            Motivos pela qual estou amando o bootcamp da DIO: 
+    </p>
+    <ul>
+        <li>Didática incrivel </li>
+        <li>Ótimos professores </li>
+        <li>E é de <b>GRAÇA</b> </li>
+    </ul>
+    </div>
+    </div>
+    </body>
+    
+    <script type= "text/javascript">
+			     $(document).ready(function()  {
+			           alert("Bem vindos");
+				 });
+	</script>
+
+     </html>
+    
